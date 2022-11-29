bdc49d9f15a980d4ff87c99b247c74018bc3d160
andreamelis2020 <69151989+andreamelis2020@users.noreply.github.com>
2022-11-29 11:41:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeng-i18n-api-with-ngx-translate-orfner
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-i18n-api-with-ngx-translate-orfner)
---
+++ src/app/app.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnDestroy } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ConfirmationService, PrimeNGConfig } from 'primeng/api';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ConfirmationService]
+})
+export class AppComponent implements OnDestroy {
+  lang: string = 'en';
+
+  date: any;
+
+  uploadedFiles: any[] = [];
+
+  password: string = '';
+
+  subscription: Subscription;
+
+  customers = [
+    { name: 'Yancey' },
+    { name: 'Chilton' },
+    { name: 'Angelo' },
+    { name: 'Carita' },
+    { name: 'Wernher' }
+  ];
+
+  constructor(
+    public translate: TranslateService,
+    public primeNGConfig: PrimeNGConfig,
+    private confirmationService: ConfirmationService
+  ) {
+    translate.addLangs(['en', 'fr']);
+    translate.setDefaultLang('en');
+
+    const browserLang = translate.getBrowserLang();
+    let lang = browserLang.match(/en|fr/) ? browserLang : 'en';
+    this.changeLang(lang);
+
+    this.subscription = this.translate.stream('primeng').subscribe(data => {
+      this.primeNGConfig.setTranslation(data);
+    });
+  }
+
+  changeLang(lang: string) {
+    this.translate.use(lang);
+  }
+
+  onUpload(event: any) {
+    for (let file of event.files) {
+      this.uploadedFiles.push(file);
+    }
+  }
+
+  confirm(event: any) {
+    this.confirmationService.confirm({
+      target: event.target,
+      message: this.translate.instant('demo.message'),
+      icon: 'pi pi-exclamation-triangle'
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
